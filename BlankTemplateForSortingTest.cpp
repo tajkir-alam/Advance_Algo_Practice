3942a699503fbff4226e630fa7dca7ed9be8d09d
tajkir-alam <tajkir.alam@gmail.com>
2025-09-12 01:17:51 +0600

    bubble sort added

--- BlankTemplateForSortingTest.cpp
+++ BlankTemplateForSortingTest.cpp
@@ -1,99 +1,124 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int ar[100004];
 int arrayCopy[100004];
 void WriteFile()
 {
-    freopen("Input.txt","w",stdout);
+    freopen("Input.txt", "w", stdout);
 
-    int N =100000;
-    cout<<N<<endl;
-    for(int i=0;i<N;i++)
+    int N = 100000;
+    cout << N << endl;
+    for (int i = 0; i < N; i++)
     {
         int x = rand();
-        cout<<x<<" ";
+        cout << x << " ";
     }
-    cout<<endl;
-
+    cout << endl;
 }
+
+float BubbleSort(int ar[], int N)
+{
+    clock_t start = clock();
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            if (ar[j] > ar[j + 1])
+            {
+                int temp = ar[j];
+                ar[j] = ar[j + 1];
+                ar[j + 1] = temp;
+            }
+        }
+    }
+    clock_t total = clock() - start;
+    cerr << "Total Time Taken in Bubble Sort is " << float(total) / 1000.0 << endl;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << ar[i] << " ";
+    }
+    cout << endl;
+};
 
 int br[100000];
-float QuickSort(int ar[],int N)
+float QuickSort(int ar[], int N)
 {
     float time = 0;
-    clock_t start= clock();
+    clock_t start = clock();
 
     /// Coding Start
-/// Finish your code within start to end
+    /// Finish your code within start to end
 
-        sort(ar,ar+N);
+    sort(ar, ar + N);
     /// Coding End
 
-
-    clock_t total=  clock() - start;
-//     time = start - ed;
-     cerr<<setprecision(10)<<fixed;
-    cerr<<"Total Time Taken in Quick Sort is "<< float(total)/1000.0<<endl;
-    for(int i=0;i<N;i++)cout<<ar[i]<<" ";
-    cout<<endl;
+    clock_t total = clock() - start;
+    //     time = start - ed;
+    cerr << setprecision(10) << fixed;
+    cerr << "Total Time Taken in Quick Sort is " << float(total) / 1000.0 << endl;
+    for (int i = 0; i < N; i++)
+        cout << ar[i] << " ";
+    cout << endl;
 }
 
-float MergeSort(int ar[],int N)
+float MergeSort(int ar[], int N)
 {
     float time = 0;
-    clock_t start= clock();
+    clock_t start = clock();
 
     /// Coding Start
 
     /// Coding End
 
-
-    clock_t total=  clock() - start;
-//     time = start - ed;
-     cerr<<setprecision(10)<<fixed;
-    cerr<<"Total Time Taken in Merge Sort is "<< float(total)/1000.0<<endl;
-    for(int i=0;i<N;i++)cout<<ar[i]<<" ";
-    cout<<endl;
+    clock_t total = clock() - start;
+    //     time = start - ed;
+    cerr << setprecision(10) << fixed;
+    cerr << "Total Time Taken in Merge Sort is " << float(total) / 1000.0 << endl;
+    for (int i = 0; i < N; i++)
+        cout << ar[i] << " ";
+    cout << endl;
 }
 
-float HeapSort(int ar[],int N)
+float HeapSort(int ar[], int N)
 {
     float time = 0;
-    clock_t start= clock();
+    clock_t start = clock();
 
     /// Coding Start
 
     /// Coding End
 
-
-    clock_t total=  clock() - start;
-//     time = start - ed;
-     cerr<<setprecision(10)<<fixed;
-    cerr<<"Total Time Taken in Heap Sort is "<< float(total)/1000.0<<endl;
-    for(int i=0;i<N;i++)cout<<ar[i]<<" ";
-    cout<<endl;
+    clock_t total = clock() - start;
+    //     time = start - ed;
+    cerr << setprecision(10) << fixed;
+    cerr << "Total Time Taken in Heap Sort is " << float(total) / 1000.0 << endl;
+    for (int i = 0; i < N; i++)
+        cout << ar[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
-    //WriteFile();
+    WriteFile();
 
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
     int N;
-    scanf("%d",&N);
-    for(int i=0;i<N;i++)
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
     {
-        scanf("%d",&ar[i]);
+        scanf("%d", &ar[i]);
     }
 
-    QuickSort(ar,N ); ///
-//    MergeSort(ar,N );
-//    HeapSort(ar,N );
-//    InsertionSort(ar,N );
+    BubbleSort(ar, N);
+    // QuickSort(ar,N ); ///
+    //    MergeSort(ar,N );
+    //    HeapSort(ar,N );
+    //    InsertionSort(ar,N );
 
     ///....
-
 
     return 0;
 }
